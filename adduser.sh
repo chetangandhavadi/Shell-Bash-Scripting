8e1a5c9bbd727820f4906b76e754fc25a3822bce
chetan gandhavadi <chetan.gandhavadi@gmail.com>
2021-01-04 15:16:32 -0500

    afas

--- adduser.sh
+++ adduser.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
-# adduser--Adds a new user to the system, including building their
-# home directory, copying in default config data, etc.
-# For a standard Unix/Linux system, not OS X.
-pwfile="/etc/passwd"
-shadowfile="/etc/shadow"
-132 Chapter 5
-gfile="/etc/group"
-hdir="/home"
-if [ "$(id -un)" != "root" ] ; then
- echo "Error: You must be root to run this command." >&2
- exit 1
+# Purpose - Script to add a user to Linux system including passsword
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p "$pass" "$username"
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system."
+	exit 2
 fi
-echo "Add new user account to $(hostname)"
-/bin/echo -n "login: " ; read login
-# The next line sets the highest possible user ID value at 5000,
-# but you should adjust this number to match the top end
-# of your user ID range.
- uid="$(awk -F: '{ if (big < $3 && $3 < 5000) big=$3 } END { print big + 1 }'\
- $pwfile)"
-homedir=$hdir/$login
-# We are giving each user their own group.
-gid=$uid
-/bin/echo -n "full name: " ; read fullname
-/bin/echo -n "shell: " ; read shell
-echo "Setting up account $login for $fullname..."
-echo ${login}:x:${uid}:${gid}:${fullname}:${homedir}:$shell >> $pwfile
-echo ${login}:*:11647:0:99999:7::: >> $shadowfile
-echo "${login}:x:${gid}:$login" >> $gfile
-mkdir $homedir
-cp -R /etc/skel/.[a-zA-Z]* $homedir
-chmod 755 $homedir
-chown -R ${login}:${login} $homedir
-# Setting an initial password
-exec passwd $login
