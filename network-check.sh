ac239e3cb38c380243ce86495027afc588bc42f6
chetan.gandhavadi@gmail.com <chetan.gandhavadi@gmail.com>
2021-01-04 22:47:05 -0500

    Mon, Jan  4, 2021 10:47:04 PM

--- network-check.sh
+++ network-check.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-ping -c 3 www.google.com > /dev/null
+# use corn tab to schedule the task in intervals 
+# If the site is down user gets email notification stating that user's website is down
+if [ $# -ne 1 ]
+then
+MYSITE=www.chetnwebsite.com
+else
+MYSITE=$1
+fi
+ping -c 3 $MYSITE > /dev/null
 if [ $? != 0 ]
 then
+echo `date +%F`
 echo "Your site seems to be down"
+mail -s "Your $MYSITE seems to be down" user@gmail.com
 fi
